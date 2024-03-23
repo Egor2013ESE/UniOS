7b22237290ab53d09575acc50c009aa90fab9b5b
Egor2013ESE <164668435+Egor2013ESE@users.noreply.github.com>
2024-03-23 20:53:43 +0300

    Add files via upload

---
+++ UniOS_(2).py
@@ -0,0 +1,143 @@
+print("Hello!")
+
+
+
+name = input("Login: ")
+password = input("Password: ")
+
+          
+print("""
+          #########
+          1/console
+          #########
+          
+          #########
+          2/servers
+          #########
+          
+          #########
+          3/purpose
+          #########
+          
+          
+                                                                 """)
+def look():
+     look1 = input(" ")
+     print(look1)
+def collect():
+          input(" ")
+def wbx():
+     look3 = input(" ")
+def nbx():
+          look4 = int(input( ))
+def lookf():
+          look5 = input( )
+def deletef():
+          print("Error")
+def createf():
+          look6 = input( )
+          look7 = input(" ")
+          print(look6, look7)
+def look111():
+          print("Error") 
+
+def num1():
+     qwert = input("Command: ")
+     print("""Commands: 
+               look
+               collect
+               wbx
+               nbx
+               look /f
+               delete /f
+               create /f
+               note### /f
+               look###
+                              """)
+def num2():
+     print("""Actions: 
+                    Search
+                    Create
+                    Join
+                                                  """)
+def search():
+          print("Error")
+def join():
+          global w, xxx
+          print(create100)
+          w = input("Действия: ")
+          if w == "Join":
+               xxx = int(input())
+               if xxx == create2:
+                    print(create3)
+def create():
+          
+          global create100, create2, create3
+          create100 = input("Name: ")
+          create2 = int(input("Password: "))
+          createx = input("Password xxx: ")
+          
+          create3 = input("Information: ")
+def purpous():
+          global thx
+          thx = input(" ")
+def exit():
+          global name, password
+          print("Hello! This OS is for hackers!")
+          name = input("Login: ")
+          password = input("Password: ")
+
+          
+          print("""
+               #########
+               1/console
+               #########
+                              (    ) NN  N   !
+               #########      (    ) N N N   !
+               2/servers      (____) N  NN   !
+               #########
+               
+               #########
+               3/purpose
+               #########
+               
+          
+                                                                 """)
+          
+          
+
+while True:
+     rew = int(input("Numder: "))
+     if rew == 1:
+          num1()
+          rew2 = int(input("Number: "))
+          if rew2 == 1:
+                         look()
+          elif rew2 == 2:
+                         collect()
+          elif rew2 == 3:
+                         wbx()
+          elif rew2 == 4:
+                         nbx()
+          elif rew2 == 5:
+                         lookf()
+          elif rew2 == 6:
+                         createf()
+          elif rew2 == 7:
+                         look111()
+          elif rew2 == 8:
+                         deletef()
+          else:
+               print("Error")
+     if rew == 2:
+          num2()
+     rew3 = int(input("Choose: "))
+     if rew3 == 1:
+                    search()
+     elif rew3 == 2:
+                    create()
+     elif rew3 == 3:
+                    join()
+                    print()
+     elif rew == 3:
+               purpous()
